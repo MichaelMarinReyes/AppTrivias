192d3f96dd8070f1eeab6363abddcf170acf9913
MichaelMarinReyes <michaelmarin201831260@cunoc.edu.gt>
2024-09-02 22:07:52 -0600

    Creación de vistas para logín y registro

--- AppMovil/app/src/main/java/com/appmovil/MainActivity.kt
+++ AppMovil/app/src/main/java/com/appmovil/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.appmovil
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,23 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+
+        val nombreText: TextView = findViewById(R.id.nombreText)
+        val iniciarBoton: Button = findViewById(R.id.iniciarSesionBoton)
+        val registrarBoton: Button = findViewById(R.id.registrarBoton)
+
+        iniciarBoton.setOnClickListener{
+            if (nombreText.getText().isEmpty()) {
+                Toast.makeText(this, "Campos vacíos", Toast.LENGTH_SHORT ).show()
+            }
+        }
+
+        registrarBoton.setOnClickListener{
+            val registro = Intent(this, Registro::class.java)
+
+            startActivity(registro)
         }
     }
 }
